155eafd8eb91f4bed639eab2cef50d51244395e4
Marcio Roberto Lima Silva <mlima-si@c2r3s1.42porto.com>
2026-08-11 17:16:23 +0100

    starting ex01 from cpp0

--- cpp0/ex01/PhoneBook.cpp
+++ cpp0/ex01/PhoneBook.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 /* Welcome to the 80s and their unbelievable technology! Write a program that behaves
 like a crappy awesome phonebook software.
 You have to implement two classes:
@@ -38,3 +40,28 @@ contact information, one field per line.
 Once a command has been correctly executed, the program waits for another one. It
 stops when the user inputs EXIT.
 Give a relevant name to your executable. */
+
+int Phonebook()
+{
+/*   It has an array of contacts.
+    ◦ It can store a maximum of 8 contacts. If the user tries to add a 9th contact,
+    replace the oldest one by the new one.
+    ◦ Please note that dynamic allocation is forbidden.
+    The contact fields are: 
+        first name
+        last name
+        nickname
+        phone number
+        darkest secret. 
+        
+    ADD()
+    SEARCH()
+    EXIT()
+    • Any other input is ignored
+    */
+}
+
+int Contact()
+{
+    /* Stands for a phonebook contact. */
+}
